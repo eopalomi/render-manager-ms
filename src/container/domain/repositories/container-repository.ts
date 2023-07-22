59ead69788136d68d418fe248fe0d3526e4607f9
Erick <eopalomi@gmail.com>
2023-07-22 14:26:05 -0500

    update response

--- src/container/domain/repositories/container-repository.ts
+++ src/container/domain/repositories/container-repository.ts
@@ -2,6 +2,6 @@ import { Container } from "../model/container.model";
 
 export interface ContainerRepository {
     find(container_id: number): Promise<Container>;
-    create(container: Container): Promise<void>;
+    create(container: Container): Promise<number>;
     update(container: Partial<Container>): Promise<void>;
 };
